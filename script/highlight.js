403bb5f6dd19716b0114847f0134f420cf29f460
Thomas Taschauer <tomtasche@gmail.com>
2011-11-14 11:26:39 +0000

    add before merge

--- script/highlight.js
+++ script/highlight.js
@@ -13,7 +13,7 @@ function setDate(date){
 
 function setTitle(title){
 	document.getElementById("heading").innerText=title;
-	document.title = title;
+	document.title = "Announcify - " + title;
 }
 
 function getTitle() {
